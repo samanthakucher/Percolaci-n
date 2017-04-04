05908042a360ca0d0e8d1fcb01e7ca4fc8655081
Samantha <samanthakucher@gmail.com>
2017-04-04 16:00:14 -0300

    Modificamos el main

--- tp1.c
+++ tp1.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-void llenar(int *red, int m, int n, float prob);
-void imprimir(int *red, int m, int n);
-int numeros(float P);
+#define P     16             // 1/2^P, P=16
+#define Z     27000          // iteraciones
+#define N     30             // lado de la red simulada
+
+
+void llenar(int *red, int n, float prob);
+void imprimir(int *red, int n);
+int numeros(float P); // meter en llenar
+int   actualizar(int *red,int *clase,int s,int frag);
+void  etiqueta_falsa(int *red,int *clase,int s1,int s2);
+void  corregir_etiqueta(int *red,int *clase,int n);
+int   percola(int *red,int n);
+
 
 int main(){
-	int *red;
-	int m, n;
-	float prob;
-	m = 10;
-	n = 9;
-	prob = 0.7;
-	srand(time(NULL));
-	red = malloc(m*n*sizeof(int));
-	llenar(red, m, n, prob);
-	imprimir(red, m, n);
+	int i, j, *red;
+	float prob, denominador;
+	n=N;
+  z=Z;
+	red = (int *)malloc(n*n*sizeof(int));
+
+  for(i=0;i<z;i++)
+    {
+      prob=0.5;
+      denominador=2.0;
+ 
+      srand(time(NULL));
+
+      for(j=0;j<P;j++) //no menos de 13 iteraciones (P>13)
+        {
+          llenar(red,n,prob);
+      
+          hoshen(red,n);
+        
+          denominador=2.0*denominador;
+
+          if (percola(red,n)) 
+             prob+=(-1.0/denominador); 
+          else prob+=(1.0/denominador);
+		//ir guardando en cada paso prob/N que percolan 
+        }
+    }
+
+  free(red);
 	return 0;
 }
 
@@ -30,16 +60,16 @@ int numeros(float P){
 	return n;
 }
  
-void llenar(int *red, int m, int n, float prob){
+void llenar(int *red, int n, float prob){
 	int i;
-	for(i=0; i<m*n; i++) {
+	for(i=0; i<n*n; i++) {
 			red[i] = numeros(prob);
 	}
 }
 
-void imprimir(int *red, int m, int n){
+void imprimir(int *red, int n){
 	int i, j;
-	for(i=0; i<m; i++){ 
+	for(i=0; i<n; i++){ 
 		for(j=0; j<n; j++){ 
 			printf("%i ", red[i*n+j]); //Si le pongo \t deja un espacio
 		}
